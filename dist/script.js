6b5e6dbb2fa02c36bcc333af4132914a9ef209ca
Mostafizur-Rahman13 <mostafiztarek@gmail.com>
2024-02-26 22:05:38 +0600

    Added Feature

---
+++ dist/script.js
@@ -0,0 +1,6 @@
+const menubtn = document.querySelector("#btn-menu")
+const slidebar = document.querySelector("#slidebar")
+
+menubtn.addEventListener("click", () => {
+    slidebar.style.display = "flex"
+})
