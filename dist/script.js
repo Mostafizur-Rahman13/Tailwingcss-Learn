d5922d9570aaf3e1ca17ad365739a86aab8836c4
Mostafizur-Rahman13 <mostafiztarek@gmail.com>
2024-02-27 21:17:45 +0600

    Added Footer

--- dist/script.js
+++ dist/script.js
@@ -4,3 +4,32 @@ const slidebar = document.querySelector("#slidebar")
 menubtn.addEventListener("click", () => {
     slidebar.style.display = "flex"
 })
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        dynanmicBullets:true,
+        clickable: true,
+    },
+    autoplay: {
+        delay : 5000,
+        clickable:true
+    },
+
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+        900: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+    },
+});
