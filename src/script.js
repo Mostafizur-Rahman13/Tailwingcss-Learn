c4b7d577299a01d3f7a73a5075393fcaf281b7e8
Mostafizur-Rahman13 <mostafiztarek@gmail.com>
2024-05-05 17:54:28 +0600

    Fixed Error

---
+++ src/script.js
@@ -0,0 +1,69 @@
+const btnmenu = document.querySelector("#btn-menu")
+const btnclose = document.querySelector("#btn-close")
+const slidebar = document.querySelector("#slidebar")
+const navcategoryid = document.querySelector("#navcategoryid")
+const categoryid = document.querySelector("#categoryid")
+
+navcategoryid.addEventListener("mouseover", () => {
+    categoryid.classList.remove("md:hidden");
+})
+
+
+
+
+setTimeout(() => {
+
+    navcategoryid.addEventListener("mouseleave", () => {
+        categoryid.classList.add("md:hidden");
+
+    })
+
+}, 2000);
+
+
+
+
+categoryid.addEventListener("mouseover", () => {
+    categoryid.classList.remove("md:hidden");
+})
+
+btnmenu.addEventListener("click", () => {
+    slidebar.style.display = "flex"
+    btnmenu.style.display = "none"
+    btnclose.style.display = "flex"
+})
+
+btnclose.addEventListener("click", () => {
+    slidebar.style.display = "none"
+    btnmenu.style.display = "flex"
+    btnclose.style.display = "none"
+})
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        dynanmicBullets: true,
+        clickable: true,
+    },
+    autoplay: {
+        delay: 5000,
+        clickable: true
+    },
+
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+        900: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+    },
+});
